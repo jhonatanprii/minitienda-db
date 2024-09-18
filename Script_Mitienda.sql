09570ba39c73ac1c9f28557f415b3f775e23c3ed
Jhonatan Prieto <jhonatan0913@gmail.com>
2024-09-18 17:14:24 -0500

    Se crea el script con los sp de la MiniTiend, de la tabla Categorias

---
+++ Script_Mitienda.sql
@@ -0,0 +1,37 @@
+-- Categorias
+-- Insertar
+DELIMITER //
+create procedure procInsertCategory(IN v_description VARCHAR(45))
+begin
+	insert into tbl_categorias(cat_descripcion) values(v_description);
+end//
+DELIMITER ;
+-- Actualizar
+DELIMITER //
+create procedure procUpdateCategory(IN v_id INT, IN v_description VARCHAR(45))
+begin
+	update tbl_categorias
+    set cat_descripcion = v_description
+    where cat_id = v_id;
+end//
+DELIMITER ;
+-- Mostrar
+DELIMITER //
+create procedure procSelectCategory()
+begin
+select cat_id, cat_descripcion from tbl_categorias;
+end//
+DELIMITER ;
+-- Eliminar
+DELIMITER //
+create procedure procDeleteCategory(IN v_id INT)
+begin
+	delete from tbl_categorias where cat_id = v_id;
+end//
+DELIMITER ;
+
+-- Proveedores
+
+-- Productos
+
+-- Usuarios
